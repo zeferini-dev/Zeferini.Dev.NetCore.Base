37e31f89a25567e7aab24f2aa8d399b12d239273
Clodoaldo Zeferini <clodoaldo.zeferini.enterprise@outlook.com>
2024-10-19 19:18:55 -0300

    Removendo GrupoOutResponse

--- src/NetCore.Base/Model/Response/OutResponse.cs
+++ src/NetCore.Base/Model/Response/OutResponse.cs
@@ -1,6 +1,5 @@
 ﻿namespace NetCore.Base.Model.Response
 {
-    public class GrupoOutResponse : ResponseBase {}
     public class AuthorizationOutResponse : ResponseBase { }
     public class LogOutResponse : ResponseBase { }
 }
