221e1631e74b58461ff7362f5053cf9551e606b4
Clodoaldo Zeferini <clodoaldo.zeferini.enterprise@outlook.com>
2024-10-19 20:38:54 -0300

    Migrando Enums

---
+++ src/NetCore.Base/Enum/EGenero.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCore.Base.Enum
+{
+    public enum EGenero
+    {
+        Feminino = 0, Masculino = 1, PrefiroNaoInformar = 2,
+    }
+}
---
+++ src/NetCore.Base/Enum/EStatusCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace NetCore.Base.Enum
+{
+    public enum EStatusCommand
+    {
+        CREATED_WITH_SUCCESS = 0,
+        FAILED_TO_CREATE     = 1,
+
+        UPDATED_WITH_SUCCESS = 2,
+        FAILED_TO_UPDATE = 3,
+
+        DELETED_WITH_SUCCESS = 4,
+        FAILED_TO_DELETE = 5,
+    }
+}
