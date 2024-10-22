f4ccc23cc16c2d06f4e5dd3fe08ff1e96695af6c
Clodoaldo Zeferini <clodoaldo.zeferini.enterprise@outlook.com>
2024-10-22 20:34:13 -0300

    Acrescentando Estruturas de Grupo até Usuário

--- src/NetCore.Base/Model/Response/OutResponse.cs
+++ src/NetCore.Base/Model/Response/OutResponse.cs
@@ -2,4 +2,11 @@
 {
     public class AuthorizationOutResponse : ResponseBase { }
     public class LogOutResponse : ResponseBase { }
+
+    public class GrupoOutResponse : ResponseBase { }
+    public class EmpresaOutResponse : ResponseBase { }
+    public class FilialOutResponse : ResponseBase { }
+    public class DepartamentoOutResponse : ResponseBase { }
+    public class PessoaOutResponse : ResponseBase { }
+    public class UsuarioOutResponse : ResponseBase { }
 }
